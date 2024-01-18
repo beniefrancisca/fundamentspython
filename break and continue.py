d9bf0c60e0dbdd4842e87ad799eccbaf98723368
beniewaku <78078820+beniewaku@users.noreply.github.com>
2024-01-18 20:47:36 +0000

    using break and continue syntax in python and how to use them

---
+++ break and continue.py
@@ -0,0 +1,18 @@
+# python keywords when used inside a loop
+cars = ["ok", "ok", "ok", "faulty", "ok", "ok"]
+
+for status in cars:
+    if status == "faulty":
+        print("stopping the production line")
+        break #this will stop the loop so when it reaches faulty it will stop the loop
+    print(f"This car is {status}")
+    print("shipping new car to customer")
+
+# if theres a faulty car but we just want to skip the line instead this is what to do
+
+for status in cars:
+    if status == "faulty":
+        print("found faulty car, skipping")
+        continue # it will jump back to the start of the loop
+    print(f"This car is {status}")
+    print("shipping new car to customer")
