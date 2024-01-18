a863f8af60e5bb3956e5c1e04f968b128ff41999
beniewaku <78078820+beniewaku@users.noreply.github.com>
2024-01-18 20:35:12 +0000

    how to iterate in dictionaries

---
+++ iterating over dictionaries.py
@@ -0,0 +1,6 @@
+# how to iterate in dictionaries
+friend_ages = {"Rolf": 25, "Anne": 37, "Charlie": 31, "Bob": 22}
+for name, age in friend_ages.items():
+    print(f"{name} is {age} years old")
+# most common way of iterating over a dictionary
+
