edbb70a5f888800d5b2844481cdf774e86bd5951
beniewaku <78078820+beniewaku@users.noreply.github.com>
2024-01-18 21:38:10 +0000

    how to use for loops to find prime numbers

---
+++ finding prime numbers with loops.py
@@ -0,0 +1,11 @@
+# finding prime numbers with for loops
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(f"{n} equals {x} * {n//x}")
+            break
+    else:
+        print(f"{n} is a prime number")
+
+# when the number is a prime number means the loop will never break
