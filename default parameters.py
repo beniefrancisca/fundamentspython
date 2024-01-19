c63c3613095fd5a1276270317440c219ef8d1a29
beniewaku <78078820+beniewaku@users.noreply.github.com>
2024-01-19 14:45:33 +0000

    default values for parameters and named parameters

---
+++ default parameters.py
@@ -0,0 +1,14 @@
+# default values for parameters
+# this is defining a function
+def add (x, y):
+    total = x + y
+    return total
+print(add(5, 10))
+
+# named arguments
+def add (x, y):
+    total = x + y
+    return total
+print(add(x=5, y=2))
+# adding value to parameters
+# this is calling the function
