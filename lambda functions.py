8632bbd537c1811242c8d893782c3ad8bc4cccbe
beniewaku <78078820+beniewaku@users.noreply.github.com>
2024-01-19 15:23:04 +0000

    howw to use lambda functions

---
+++ lambda functions.py
@@ -0,0 +1,15 @@
+# lambda functions are used to get inputs do a small amount of processing
+# and return outputs
+# functions can peform an action or they can return an output
+
+def divide(x, y):
+    return x / y
+
+
+
+divide = lambda x, y: x/ y
+#doing this means you don't need the code above ^^^^^
+# lambda functions provide more simplicity to codes or sometimes make it complicated
+
+
+
