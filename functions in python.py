b17d218315f4aab334517f2a30c5f0c34e46b706
beniewaku <78078820+beniewaku@users.noreply.github.com>
2024-01-19 13:34:57 +0000

    how to use def function

---
+++ functions in python.py
@@ -0,0 +1,7 @@
+# functions only start with the def keyword
+
+def greet():
+    name = input("Enter your name: ")
+    print(f"Hello, {name}")
+greet() #calling the function name outside the body will run the function
+
